15c24cdbba32df3b0dd7897c48206a02a82e8414
ADITYA SINGH <sadityasingh7990@gmail.com>
2026-01-18 13:20:36 +0530

    Initial Commit

---
+++ Offline Guardian/Offline_GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Offline_GuardianApp.swift
+//  Offline Guardian
+//
+//  Created by Aditya Singh on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Offline_GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
